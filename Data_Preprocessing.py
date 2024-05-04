5b3691b5cbb1a1454dfa1050f5a6643b37f49566
Raneen Younis <younis@l3s.de>
2024-05-04 09:49:21 +0200

    Upload New File

---
+++ Data_Preprocessing.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 10:39:50 2020
+
+@author: raneen_pc
+
+This class is responsible for reading a time series data and preprocessing it 
+and return a dictionary that contains the original data split into training 
+and testing sets
+"""
+
+
+import numpy as np
+import scipy.io as sio
+from scipy.interpolate import interp1d
+
+
+class ReadData:
+    def __init__(self):  
+        self = self
+    
+
